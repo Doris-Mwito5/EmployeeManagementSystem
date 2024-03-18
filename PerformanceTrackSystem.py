cb0dd5d8069dfe82611c442d70891c11d96278b6
Doris-Mwito5 <kawiramwito5@gmail.com>
2024-03-18 11:55:01 +0300

    added an employee performance module

---
+++ PerformanceTrackSystem.py
@@ -0,0 +1,28 @@
+
+class Peformance:
+    def track(self,employees,hours):
+        print("Employees Performance")
+        print("=====================")
+        
+        for employee in employees:
+            status = employee.work(hours)
+            print(f"{Employee.name} : {status}")
+            print("===========================")
+        
+class ManagerRole:
+    def track(self,hours):
+        return f"The manager is handling the team for {hours} hours"
+    
+class DeveloperRole:
+    def track(self,hours):
+        return f"The developer has been working for {hours} hours"
+    
+class SalesRole:
+    def track(self,hours):
+        return f"The salesperson will be working for {hours} hours"
+    
+class WorkerRole:
+    def track(self,hours):
+        return f"All casual workers are supposed to work for {hours} hours"
+    
+               
